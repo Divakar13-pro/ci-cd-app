5eadd4ef60ed6cc1d7315a6946f3ec485c3b6e41
divakar1302 <divakarkumarktr@gmail.com>
2025-09-04 07:24:49 +0530

    Testing CI/CD pipeline with code change

--- server.test.js
+++ server.test.js
@@ -6,5 +6,5 @@ app.get("/", (req, res) => res.send("Hello DevOps - CI/CD Pipeline works!"));
 
 test("GET / should return hello message", async () => {
   const res = await request(app).get("/");
-  expect(res.text).toBe("Hello DevOps - CI/CD Pipeline works! hhhhhhh");
+  expect(res.text).toBe("Hello DevOps - CI/CD Pipeline works!");
 });
